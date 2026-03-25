e0bb8f54c4ce20fa2f6632096ce321aa4149f8e3
AleksanderSK1 <alkna009@osloskolen.no>
2026-03-25 17:46:59 +0100

    Oppdateret server.js

--- server.js
+++ server.js
@@ -13,3 +13,31 @@ function lesData() {
 function skrivData(data) {
   fs.writeFileSync(DATA_FIL, JSON.stringify(data, null, 2));
 }
+
+app.get("/notater", (req, res) => {
+    const data = lesData();
+    res.json(data.notater);
+  });
+  
+  app.post("/notater", (req, res) => {
+    const data = lesData();
+    data.notater.push(req.body);
+    skrivData(data);
+    res.send("lagret");
+  });
+  
+  app.get("/todolister", (req, res) => {
+    const data = lesData();
+    res.json(data.todolister);
+  });
+  
+  app.post("/todolister", (req, res) => {
+    const data = lesData();
+    data.todolister.push(req.body);
+    skrivData(data);
+    res.send("lagret");
+  });
+  
+  app.listen(8000, "0.0.0.0", () => {
+    console.log("Server kjører på port 8000");
+  });
